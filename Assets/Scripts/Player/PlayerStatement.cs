449cf293111364c6f1560eb834a6ac68c6977e51
smwater <smwater130@naver.com>
2022-07-12 18:41:45 +0900

    플레이어 죽음 구현

---
+++ Assets/Scripts/Player/PlayerStatement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatement : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Bullet")
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
